5651c6b574e62cd4b78841cb1afaeb78e0ec7290
talu <talita.santos96@hotmail.com>
2023-12-02 18:30:48 -0300

    add Artist class

---
+++ classes/Artist.js
@@ -0,0 +1,18 @@
+import { User } from './User.js';
+
+export class Artist {
+    #graffitiStyle;
+    static allArtists = [];
+
+    getAllArtists() {
+        this.artists;
+    }
+
+    get graffitiStyle() {
+        this.#graffitiStyle;
+    }
+
+    set graffitiStyle(newGraffitiStyle) {
+        this.#graffitiStyle = newGraffitiStyle;
+    }
+}
