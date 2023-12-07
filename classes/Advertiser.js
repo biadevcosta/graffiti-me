17862e51fc43d507d4dbf33d47881da9caa6cb08
talu <talita.santos96@hotmail.com>
2023-12-06 21:35:45 -0300

    adiciona teste de console na classe Advertiser

--- classes/Advertiser.js
+++ classes/Advertiser.js
@@ -1,11 +1,27 @@
-const User = require ('./User.js');
+const User = require('./User.js');
 
 class Advertiser extends User {
     static allAdvertisers = [];
+
+    constructor(name, profile, email, password, dateOfBirth, CPF, description, postalCode) {
+        super(name, profile, email, password, dateOfBirth, CPF, description, postalCode);
+    }
 
     getAllAdvertisers() {
         this.advertisers;
     }
 }
+
+const advertiser1 = new Advertiser('Talita Santos',
+    2,
+    'contato.talita@gmail.com',
+    'SenhaF@rte123',
+    '23/12/1996',
+    '51984064819',
+    'Busco artistas.',
+    '06815435');
+
+    console.log(advertiser1);
+    console.log(advertiser1.name)
 
 module.exports = Advertiser;
